96f4b9063f360604e2e2b5a3cd12b1f8c5b22a8e
Karim Daw <karimdaw91@gmail.com>
2022-02-16 22:13:38 +0100

    add age

--- src/Home.js
+++ src/Home.js
@@ -2,18 +2,20 @@ import { useState } from "react"
 
 
 const Home = () => {
-    // using hooks to change values upon events
+    // using hooks to change state upon events
     const [name, setName] = useState("Karim")
+    const [age, setAge] = useState(30)
 
     // using setName to change to new name 
     const handleClick = (name) => {
         setName("Ibrahim")
+        setAge(26)
     }
-    
+
     return ( 
         <div className="home">
             <h2>Homepage</h2>
-            <p>{ name }</p>
+            <p>My name is { name } and I am {age} years old</p>
             <button onClick={handleClick}>Click me!</button>
         </div>
      );
