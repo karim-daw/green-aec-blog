66531589df379b172a103185bf7b5b4498a210ae
Karim Daw <karimdaw91@gmail.com>
2022-02-17 22:59:18 +0100

    using dependancy array

--- src/Home.js
+++ src/Home.js
@@ -1,6 +1,5 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import BlogList from "./BlogList";
-
 
 const Home = () => {
     // using hooks to change state upon events
@@ -10,11 +9,22 @@ const Home = () => {
         { title: "Food Preference", body: "I Like pizza!", author: "Cherine", id: 2},
         { title: "Travel Plans", body: "I am goign to paris tomorrow", author: "Ibrahim", id: 3}
     ])
+    
+    const [name, setName] = useState("Karim")
 
+    // function that handles delete, is then used as a prop for BlogLists
     const handleDelete = (id) => {
         const newBlogs = blogs.filter((blog) => blog.id !== id)
         setBlogs(newBlogs)
     }
+
+    // this function will fire at EVERY render
+    // you can add a dependancy array
+    // if array is empty [] it will only fire once in teh beggining
+    // if you put something in the array [ name ] it will fire if name changes state
+    useEffect(() => {
+        console.log("use effect ran")
+    }, [name])
 
     return ( 
         // blogs={blogs} is a a prop
