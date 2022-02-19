4de20ea45b22fc66d4db5abc2ee503b8a9dd4ebf
Karim Daw <karimdaw91@gmail.com>
2022-02-19 20:31:37 +0100

    added comment to save command for json server

--- src/Home.js
+++ src/Home.js
@@ -1,19 +1,13 @@
 import { useState, useEffect } from "react"
 import BlogList from "./BlogList";
 
+// starting a jsonserver
+// npxjson-server --watch data/db.json --port 8000
+
 const Home = () => {
     // using hooks to change state upon events
     const [blogs, setBlogs] = useState(null)
     
-    const [name, setName] = useState("Karim")
-
-    // function that handles delete, 
-    // it is then used as a prop for BlogLists
-    const handleDelete = (id) => {
-        const newBlogs = blogs.filter((blog) => blog.id !== id)
-        setBlogs(newBlogs)
-    }
-
     // this function will fire at EVERY render
     // you can add a dependancy array
     // if array is empty [] it will only fire once in teh beggining
@@ -36,8 +30,10 @@ const Home = () => {
         // blogs is being given 3 sets of props
         // each prop is different, blogs is array of data
         // title is just a string, and handleDelete is a function itself 
+        // && is conditional templateing in javascript
+        // if left of && is false, doesnt output what is to the right of it
         <div className="home">
-            {blogs &&<BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete} />}
+            {blogs && <BlogList blogs={blogs} title="All Blogs" />}
         </div>
      );
 }
