f520191d5aa2ee1e72ec854019f6d2703c08cb0a
karimdaw1991 <karimdaw91@gmail.com>
2022-02-26 22:57:43 +0100

    delete blogs

--- src/BlogList.js
+++ src/BlogList.js
@@ -1,12 +1,10 @@
 import { Link } from "react-router-dom";
 
-
 // blogs and title are props that have data
 // handleDelete is a prop that is a function that handles delete
 const BlogList = ({ blogs, title}) => {
     // const blogs = props.blogs;
     // const title = props.title;
-
     // use map() function to itereate over array and manipute data rendered to DOM
     return (
       <div className="blog-list">
@@ -17,7 +15,6 @@ const BlogList = ({ blogs, title}) => {
               <h2>{ blog.title }</h2>
               <p>Written by { blog.author }</p>
             </Link>
-
           </div>
         ))}
       </div>
