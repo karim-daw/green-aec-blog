f57f5f389e4f8bb6becf7c137e0333599fe46630
karimdaw1991 <karimdaw91@gmail.com>
2022-02-28 21:22:03 +0100

    add config file

---
+++ src/Profile.js
@@ -0,0 +1,10 @@
+const Profile = () => {
+    return (  
+        <>
+            <h1>login page</h1>
+        </>
+
+    );
+}
+ 
+export default Profile;
---
+++ src/config/constant.js
@@ -0,0 +1,1 @@
+export const API_SERVER = "https://fastapi-karim.herokuapp.com/"
