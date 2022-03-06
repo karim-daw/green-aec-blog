725cb35de71016c9dc012e77a5ff50bddee35030
karimdaw1991 <karimdaw91@gmail.com>
2022-03-06 23:51:25 +0100

    added more stuff

--- src/services/User.js
+++ src/services/User.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import authHeader from './auth-header';
+import authHeader from './Auth-header';
 const API_URL = "https://fastapi-karim.herokuapp.com/"
 
 class UserService {
